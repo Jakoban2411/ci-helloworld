ec0d2cf74e844c99b5a2f3bb4e00564d00b07346
Konisko Bandyopadhyay <30558121+Jakoban2411@users.noreply.github.com>
2024-03-31 13:04:47 -0400

    Adding Test case of Countof

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -46,4 +46,9 @@ public class ArrayUtilsTest {
     public void testOddOrPositiveBothPositivesAndNegatives() {
         assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
     }
+
+    @Test
+    public void tetsCountZero() {
+        assertEquals(1, ArrayUtils.countOf(new int[]{-3, -2, 0, 1, 4},0));
+    }
 }
